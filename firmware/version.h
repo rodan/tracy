0611b8bbded977601f70175966e6989084e5bba3
Petre Rodan <c59ccef70aa1387612c4@subdimension.ro>
2019-05-01 09:08:10 +0300

    latest

--- firmware/version.h
+++ firmware/version.h
@@ -1,9 +1,9 @@
 #ifndef __VERSION_H__
 #define __VERSION_H__
 
-// compiled on Sun Jan 13 14:30:52 UTC 2019
+// compiled on Thu Mar 28 12:59:28 UTC 2019
 
-#define BUILD 408
-#define COMMIT 159
+#define BUILD 411
+#define COMMIT 160
 
 #endif
